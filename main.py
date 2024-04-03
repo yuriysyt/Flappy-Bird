2ba408d89a19567a73d604f4fbaebc5ed839d6c2
yuriysyt <162964888+yuriysyt@users.noreply.github.com>
2024-04-03 16:04:58 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame
+
+
+class GameLoop:
+    width, height = 1280,720
+    running = False
+    dt = 0
+
+    def __init__(self):
+        pass
+
+    def input(self):
+        pass
+                
+
+    def game_loop(self):
+        while self.running:
+            pass
+
+
+if __name__ == "__main__":
+    game = GameLoop()
+    game.game_loop()
+
+pygame.quit()
