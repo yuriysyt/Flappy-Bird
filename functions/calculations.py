d1f23bed4d0cf89f3c2ffd6654b8403220cd1436
yuriysyt <162964888+yuriysyt@users.noreply.github.com>
2024-04-07 11:14:53 +0100

    Added 'create_level' method to draw game obstacles
    Introduced a create_level() method in the 'calculations' class within 'calculations.py' to draw game obstacles based on level specifications.
    
    Within the create_level() method:
    Iterated through the positions specified in maps.pos to determine the position of each obstacle.
    
    Checked if the position of the obstacle falls within the visible range (-400 to screen.get_height() + 1000) to avoid unnecessary rendering.
    
    Retrieved parameters such as width, height, and up-down movement from maps.width, maps.height, and maps.updown, respectively, based on the current position index (ipos).
    
    Scaled the tube image (images.tubeImg) and its rotated version (images.rotateTubeImg) based on the width and height parameters.
    
    Positioned the scaled image based on the obstacle's position (pos) and the current tube position (self.tube_pos).
    
    Adjusted the vertical position based on whether the obstacle moves up or down (updown).
    
    Rendered the scaled image onto the screen at the calculated position.
    
    Called the lose_game() method to check for collisions between the player and the obstacle, passing the scaled image and screen position as parameters.

--- functions/calculations.py
+++ functions/calculations.py
@@ -58,6 +58,22 @@ class calculations:
         if self.tree_pos[0] < -300:
             self.tree_pos[0] = (self.screen.get_width()) + 50
 
+    def create_level(self):
+        for ipos in range(len(maps.pos)):
+            pos = maps.pos[ipos]
+            if pos + (self.tube_pos[0]) > -400 and pos + (self.tube_pos[0]) < self.screen.get_height() + 1000:
+                width = maps.height[ipos]
+                updown = maps.updown[ipos]
+                height = maps.height[ipos]
+                param_scale = width, height
+                scaled_image = pygame.transform.scale(images.tubeImg, param_scale)
+                scaled_image_rotate = pygame.transform.scale(images.rotateTubeImg, param_scale)
+                scaled_image = scaled_image if updown == 0 else scaled_image_rotate
+                screen_position = (pos + self.tube_pos[0], self.screen.get_height() - 200 if updown == 0 else self.screen.get_height() / 1500)
+                self.screen.blit(scaled_image, screen_position)
+                calculations.lose_game(self, scaled_image, screen_position)
+
+
     def create_background(self):
         for ipos in range(len(maps.pos)):
             current_pos = (400 * ipos) + self.background_pos[0]
