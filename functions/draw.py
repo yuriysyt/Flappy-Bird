63b71aa529c338f2cd84667fe3571ea38a27389b
yuriysyt <162964888+yuriysyt@users.noreply.github.com>
2024-04-07 11:22:29 +0100

    Updated 'draw.py' with additional element drawing
    Added a loop to draw ground elements multiple times to cover the visible range of the screen.
    
    Iterated over a range from -2 to 6 to determine the number of ground elements to draw.
    
    Blitted the ground image (images.groundImg) onto the screen at different positions along the x-axis, calculated based on the ground position (self.ground_pos) and the current iteration.
    
    Called the create_level() method from the 'calculations' class to draw game obstacles based on the level specifications.

--- functions/draw.py
+++ functions/draw.py
@@ -8,11 +8,15 @@ class DrawingTool():
         self.screen.fill('#4ec0ca')
 
         calculations.create_background(self)
-
-        
         
         if self.is_jumping:
             self.screen.blit(images.upCarImg, self.player_pos)
         else:
             self.screen.blit(images.downCarImg, self.player_pos)
+
+        for i in range(-2, 7):
+            self.screen.blit(images.groundImg, (self.ground_pos.x + 300 * i, self.ground_pos.y))
+           # self.screen.blit(images.skyImg, (self.sky_pos.x + 600 * i, self.sky_pos.y))
+
+        calculations.create_level(self)
        
