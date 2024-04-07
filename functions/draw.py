afd11dccc95c2646bde9b8af95cddafa041b3ad7
yuriysyt <162964888+yuriysyt@users.noreply.github.com>
2024-04-07 11:20:05 +0100

    Updated 'draw.py' with 'draw' method for drawing elements
    Added a draw() method to the 'DrawingTool' class within 'draw.py' to handle drawing elements on the screen.
    
    Within the draw() method:
    
    Filled the screen with a cyan color using self.screen.fill('#4ec0ca') to create the background.
    
    Called the create_background() method from the 'calculations' class to draw the background elements.
    
    Checked if the player is jumping (self.is_jumping) and accordingly blitted the player's image (images.upCarImg) onto the screen at the player's position (self.player_pos), or if not jumping, blitted the player's image (images.downCarImg) onto the screen at the player's position.

--- functions/draw.py
+++ functions/draw.py
@@ -4,4 +4,15 @@ import pygame
 from functions.calculations import calculations
 
 class DrawingTool():
-    pass
+    def draw(self):
+        self.screen.fill('#4ec0ca')
+
+        calculations.create_background(self)
+
+        
+        
+        if self.is_jumping:
+            self.screen.blit(images.upCarImg, self.player_pos)
+        else:
+            self.screen.blit(images.downCarImg, self.player_pos)
+       
