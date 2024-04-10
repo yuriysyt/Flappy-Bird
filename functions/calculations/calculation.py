5a23ff1ce370ae2cc675aa46f2247757beca2e3d
yuriysyt <162964888+yuriysyt@users.noreply.github.com>
2024-04-10 16:49:23 +0100

    Moved the initialization of variables back to 'calculations.py'
    • Moved the initialization of variables such as losing, winning, is_jumping, etc., back to the init method within the calculations class in 'calculations.py'.
    
    • Removed these variable initializations from the __init__ method of the GameLoop class in 'main.py'.
    
    • Now, the GameLoop class only calls the init method from the calculations class to initialize the variables before starting the game loop.

--- functions/calculations/calculation.py
+++ functions/calculations/calculation.py
@@ -1,10 +1,31 @@
+from func_image.find_img import images
 import pygame
 from functions.calculations.score_calculator import ScoreCalculator
 from levels.level_1 import maps
 
 class calculations:
+
+    def init(self):  
+            self.losing = False
+            self.winning = False
+            self.is_jumping = False
+            self.current_time = pygame.time.get_ticks()
+            self.time_start_game = pygame.time.get_ticks()
+            self.clock = pygame.time.Clock()
+            self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+            pygame.init()
+            self.running = True 
+            self.last_score = 0
+            self.down_time = 0  
+            self.dt = self.clock.tick(60) / 1000
+            self.gravity = 0.2
+            self.jump_strength = -0.3
+            self.fall_speed = 0.2
+
+            self.player_pos, self.ground_pos, self.tree_pos, self.sky_pos, self.tube_pos, self.background_pos, self.gameover_pos, self.welcome_pos, self.win_pos = images.get_pos(self.screen)
         
     def calculate(self):
+            
         keys = pygame.key.get_pressed()
         
         if not self.losing and not self.winning:
@@ -49,6 +70,16 @@ class calculations:
         if self.tree_pos[0] < -300:
             self.tree_pos[0] = (self.screen.get_width()) + 50
     
+    def input(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN and self.losing or self.winning:
+                self.player_pos, self.ground_pos, self.tree_pos, self.sky_pos, self.tube_pos, self.background_pos, self.gameover_pos, self.welcome_pos, self.win_pos = images.get_pos(self.screen)
+                self.time_start_game = pygame.time.get_ticks()
+                self.losing, self.winning = False, False
+
     def lose_game(self, image_rect, player_pos):
         player_rect = pygame.Rect(player_pos[0], player_pos[1], 100, 50) 
         if image_rect.colliderect(player_rect):
--- main.py
+++ main.py
@@ -4,44 +4,15 @@ from functions.calculations.calculation import calculations
 from functions.draw import DrawingTool
 
 class GameLoop:
-    width, height = 1280,720
-    running = False
-
     def __init__(self):
-
-        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-        self.clock = pygame.time.Clock()
-        pygame.init()
-        self.running = True   
-        self.losing = False
-        self.winning = False
-        self.is_jumping = False
-        self.current_time = pygame.time.get_ticks()
-        self.time_start_game = pygame.time.get_ticks()
-        self.last_score = 0
-        self.down_time = 0  
-        self.dt = self.clock.tick(60) / 1000
-        self.gravity = 0.2
-        self.jump_strength = -0.3
-        self.fall_speed = 0.2
-
-        self.player_pos, self.ground_pos, self.tree_pos, self.sky_pos, self.tube_pos, self.background_pos, self.gameover_pos, self.welcome_pos, self.win_pos = images.get_pos(self.screen)
-
-    def input(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.running = False
-
-            if event.type == pygame.MOUSEBUTTONDOWN and self.losing or self.winning:
-                self.player_pos, self.ground_pos, self.tree_pos, self.sky_pos, self.tube_pos, self.background_pos, self.gameover_pos, self.welcome_pos, self.win_pos = images.get_pos(self.screen)
-                self.time_start_game = pygame.time.get_ticks()
-                self.losing, self.winning = False, False
+        self.running = True 
+        calculations.init(self)
                 
 
     def game_loop(self):
         while self.running:
-            self.input()
             calculations.calculate(self)
+            calculations.input(self)
             DrawingTool.draw(self)
 
 
